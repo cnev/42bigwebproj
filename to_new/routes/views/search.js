2a5d9fb04d34689406d2a31c1bd1f31372dffec4
Chandara NEV <cnev@e3r2p6.42.fr>
2015-07-02 17:09:50 +0200

    search ... kind of (besoin de corriger le filter ldap

--- to_new/routes/views/search.js
+++ to_new/routes/views/search.js
@@ -4,41 +4,43 @@ var ldap = require('ldapjs');
 
 var router = express.Router();
 
-router.get('/', function (req, res) {
-			var test = "<div><form action='/login' method='POST'><label for='username'>Username:</label><input type='text' id='username' name='username'><label for='password'>Password:</label><p><a href='#'>Forgot your password?</a></p><input type='password' id='password' name='password'><div><input type='checkbox'><label class='check' for='checkbox'>Keep me logged in</label><input type='submit' value='Login'></div></form></div>";
-			res.status(200).send(test);
-
-	});
-
 router.post('/', function (req, res) {
 	var client = ldap.createClient({
 	url: 'ldap://ldap.42.fr:389'
 });
+	//var filter = "'(uid=*"+req.body.username+"*)'";
+	//console.log("testing filter: "+filter);
+	/*var f = new ApproximateFilter({
+  		attribute: 'cn',
+ 		value: 'foo'
+	});*/
+	console.log('(|((uid=*'+req.body.username+'*)(cn=*'+req.body.username+'*)(sn=*'+req.body.username+'*)(given-name=*'+req.body.username+'*)))');
 	var opts = {
-	filter: '(&(uid='+req.body.username+'))',
+	filter: '(&(givenName=*'+req.body.username+'*))',
 	scope: 'sub',
-	attributes: 'dn'
+	attributes: 'uid'
 };
-	var user_dn = '';
+	//var user_dn = '';
+	//console.log('(uid=*'+req.body.username+'*)');
+	var to_send = '';
 	client.search('ou=paris,ou=people,dc=42,dc=fr', opts, function(err, result) {
 	//assert.ifError(err);
 
 	result.on('searchEntry', function(entry) {
 		//console.log('entry: ' + JSON.stringify(entry.object));
 		//console.log('test: ' + entry.object.dn);
-		client.bind(entry.object.dn, req.body.password, function(err) {
+
+		client.bind(req.session.dn, req.session.pw, function(err) {
 		if (err) {
 			// error case;	assert.ifError(err);
-			res.status(200).send("<p>Hello World of FAILED LOGINS</p>");
+			res.status(200).send("<p>Hello World of FAILED SEARCHES</p>");
 
 		}
 		else {
-			var sess = req.session;
-			sess.user = req.body.username;
-			sess.pw = req.body.password;
-			sess.dn = entry.object.dn;
-			sess.logged = 'true';
-			res.redirect("/");
+
+			to_send += "<p>"+entry.object.uid+"</p><br />";
+			console.log(entry.object.uid);
+
 		}
 		});
 	});
@@ -52,10 +54,7 @@ router.post('/', function (req, res) {
 		console.log('status: ' + result.status);
 	});
 });
-
-
-
+	res.status(200).send(to_send);
 })
 
 module.exports = router;
-
--- to_new/routes/views/test.js
+++ to_new/routes/views/test.js
@@ -8,7 +8,12 @@ router.get('/', function (req, res) {
 	//res.status(200).send(test);
 	var sess = req.session;
 	if (sess.logged == 'true')
-		var test2 = "<a href='logout'>LOG OUT</a>";
+	{
+		var test2 = '';
+		test2 += "<div><p>Hello "+sess.user+" !</p></div>";
+		test2 += "<div><form action='/search' method='POST'><label for='username'>Username:</label><input type='text' id='username' name='username'><input type='submit' value='Login'></form></div>";
+		test2 += "<div><a href='logout'>LOG OUT</a></div>";
+	}
 	else
 		var test2 = "<a href='login'>LOG IN</a>";
 	res.status(200).send(test2);
