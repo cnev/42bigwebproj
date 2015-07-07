66f7634c49cfb66bfeb7cce75c87fcf07c396721
Kameryu <sorin.mickael@gmail.com>
2015-07-07 16:47:59 +0200

    truc

--- models/Ticket.js
+++ models/Ticket.js
@@ -17,7 +17,7 @@ Ticket.add({
 	title: { type: String, initial: 'title', required: true },
 	state: { type: Types.Select, options: 'new, open, close', default: 'open' },
 	author: { type: Types.Relationship, ref: 'User' },
-	publishedDate: { type: Types.Date },
+	publishedDate: { type: Types.Date, default: Date.now },
 	content: { type: Types.Textarea },
 	category: { type: Types.Relationship, ref: 'TicketCategory' },
 	priority: { type: Types.Select, options: 'Non Important, Normal, Important, Urgent, Over 9000', default: 'Normal' }
