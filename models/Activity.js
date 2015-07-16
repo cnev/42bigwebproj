216f2c12dbd2f6f71b4b78e04add115299dc3206
cnev <orthana103@gmail.com>
2015-07-16 10:41:50 +0200

    x

--- models/Activity.js
+++ models/Activity.js
@@ -9,7 +9,21 @@ var Types = keystone.Field.Types;
 var Activity = new keystone.List('Activity');
 
 Activity.add({
-	name: { type: Types.Name, required: true, index: true },
+	name: {type: Types.Name, required: true, index: true},
+	description: {type: Types.Textarea, required: true},
+	subject: {type: String, required: true},
+	/* slots devrait etre un objet contenant deux valeurs 'actuel' et 'max' */
+	slots: {type: Types.Number, required: true, default: 424242},
+	/* pareil, 'begins' et 'ends' */
+	register_period: {type: Types.Date, required: true},
+	/* pareil */
+	period: {type: Types.Date, required: true},
+	/* ... */
+	group_size: {type: Types.Date, required: true},
+	req_corrections: {type: Types.Number, required: true},
+	auto_group: {type: Types.Boolean, required: true},
+	module: {type: Types.Relationship, ref: 'Module', required: true},
+	type: {type: Types.Select, options: 'project, exam, td'}
 });
 
 /**
--- models/Module.js
+++ models/Module.js
@@ -10,8 +10,14 @@ var Module = new keystone.List('Module');
 
 Module.add({
 	name: {type: Types.Name, required: true, index: true},
-	description: {type: Types.Text},
-
+	description: {type: Types.Textarea, required: true},
+	/* slots devrait etre un objet contenant deux valeurs 'actuel' et 'max' */
+	slots: {type: Types.Number, required: true, default: 424242},
+	/* pareil, 'begins' et 'ends' */
+	register_period: {type: Types.Date, required: true},
+	/* pareil */
+	period: {type: Types.Date, required: true},
+	credits: {type: Types.Number, required: true}
 });
 
 
