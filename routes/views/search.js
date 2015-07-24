3d1ca80581ce95b558c5dc39656800488fe30a8a
Mickael SORIN <msorin@e3r8p10.42.fr>
2015-07-24 15:54:28 +0200

    qwerty

--- routes/views/search.js
+++ routes/views/search.js
@@ -13,9 +13,9 @@ var parsing = ldap.parseFilter;
 //var filter = "'(uid=*"+req.body.search+"*)'";
 //console.log("testing filter: "+filter);
 /*var f = new ApproximateFilter({
-attribute: 'cn',
-value: 'foo'
-});*/
+  attribute: 'cn',
+  value: 'foo'
+  });*/
 
 //var user_dn = '';
 //console.log('(uid=*'+req.body.search+'*)');
@@ -33,27 +33,26 @@ var client = ldap.createClient({
 
 router.post('/', function (req, res) {
 	/*var client = ldap.createClient({
-		url: 'ldap://ldap.42.fr:389'
-	});*/
+	  url: 'ldap://ldap.42.fr:389'
+	  });*/
 	var view = new keystone.View(req, res);
 	console.log(client);
 	console.log('(|((uid=*'+req.body.search+'*)(cn=*'+req.body.search+'*)))');
 	var opts = {
 		filter: '(|(uid=*'+req.body.search+'*)(cn=*'+req.body.search+'*)(sn=*'+req.body.search+'*)(given-name=*'+req.body.search+'*))',
-		scope: 'sub'
-		//attributes: 'uid'
+	scope: 'sub'
+	//attributes: 'uid'
 	};
 	var to_send = '';
 	client.search('ou=paris,ou=people,dc=42,dc=fr', opts, function(err, result) {
 		if (err) {
 			console.error('qwertyuiop');
-		assert.ifError(err);
-
+			assert.ifError(err);
 		}
 		else {
 
 
-			console.log(result)
+			//console.log(result);
 
 			result.on('searchEntry', function(entry) {
 				client.bind(req.session.dn, req.session.pw, function(err) {
@@ -63,12 +62,21 @@ router.post('/', function (req, res) {
 					}
 					else {
 						for (var key in entry.object)
-							to_send += key + "<br />";
-						console.log('entry.object.uid - ' + entry.object.uid);
-						console.log('entry.object.givenName - ' + entry.object.givenName);
-						console.log('entry.object.sn - ' + entry.object.sn);
-						view.render('search');
-						//res.status(200).send(entry.object);
+					to_send += key + "<br />";
+				console.log(to_send);
+				console.log('entry.object.uid - ' + entry.object.uid);
+				console.log('entry.object.givenName - ' + entry.object.givenName);
+				console.log('entry.object.sn - ' + entry.object.sn);
+				var photofile = entry.object.uid + '.jpeg';
+				var photofold = '/nfs/zfs-student-4/users/2013/msorin/Documents/Travail/Groupe/42_Big_Web_Project/' + photofile;
+				var fd = fs.openSync(photofold, 'w');
+				fs.writeSync(fd, entry.object.jpegPhoto);
+				fs.closeSync(fd);
+				var to_ret = entry.object;
+				to_ret.photophoto = photofold;
+				res.locals.seeking = to_ret;
+				view.render('search');
+				//res.status(200).send(entry.object);
 					}
 				});
 			});
