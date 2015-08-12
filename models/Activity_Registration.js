0d2023f63642e9dc169fb71b84f2b7c0ebf92b0c
Mickael SORIN <msorin@e1r12p10.42.fr>
2015-08-12 15:22:30 +0200

    qwerty

--- models/Activity_Registration.js
+++ models/Activity_Registration.js
@@ -1,7 +1,12 @@
 var keystone = require('keystone');
 var Types = keystone.Field.Types;
 
-var ActivityRegistration = new keystone.List('ActivityRegistration');
+var ActivityRegistration = new keystone.List('ActivityRegistration', track {
+	createdAt: true,
+	createdBy: true,
+	updatedAt: true,
+	updatedBy: true
+});
 
 ActivityRegistration.add({
 	user: {type: Types.Relationship, ref: 'User'},
