4f87d5f14a1a2ddc53b3da4ba109f0062d7b00a1
Chandara NEV <cnev@e1r12p10.42.fr>
2015-08-19 16:31:22 +0200

    x

--- routes/views/schedule.js
+++ routes/views/schedule.js
@@ -4,11 +4,19 @@ var keystone = require('keystone');
 var schedule = require('node-schedule');
 var router = express.Router();
 
+var ActRDrv = require('../../driver/actRegisDriver').ActRegisDriver;
+var ActRegisDriver = new ActRDrv ();
+
 router.get('/', function (req, res) {
-	var j = schedule.scheduleJob('*/1 * * * *', function() {
+	var job_timer = schedule.scheduleJob('*/1 * * * *', function() {
 		console.log('The answer to life, the universe, and everything!');
 		var date = new Date;
 		console.log('this prints on '+date.toLocaleString());
+	});
+	var job_end_activities = schedule.scheduleJob('*/1 * * * *', function() {
+		ActRegisDriver.end_activities(function (err, end){
+
+		});
 	});
 	req.flash('info', 'schedule is ON');
 	res.redirect('/admin');
